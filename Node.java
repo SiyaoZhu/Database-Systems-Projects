7574fc650622bbed0066c4f7d3bfc4bdcc66d3d3
SiyaoZhu <sz427@cornell.edu>
2016-06-22 23:55:51 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Node<K extends Comparable<K>, T> {
+	protected boolean isLeafNode;
+	protected ArrayList<K> keys;
+	
+	public String toString(){
+		return this.isLeafNode+" "+keys.size();
+	}
+
+	public boolean isOverflowed() {
+		return keys.size() > 2 * BPlusTree.D;
+	}
+
+	public boolean isUnderflowed() {
+		return keys.size() < BPlusTree.D;
+	}
+
+}
